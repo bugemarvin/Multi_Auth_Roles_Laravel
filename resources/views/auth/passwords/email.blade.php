aec6ab796c4d6fc481106a258197c502e8720cfc
Marvin Kurland <bugemarvin@outlook.com>
2021-08-06 21:02:43 +0300

    reset password update

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -7,6 +7,7 @@
 	<meta name="viewport" content="width=device-width,initial-scale=1">
 	<title>Reset Password</title>
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"  crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/my-login.css">
 </head>
 <body class="my-login-page">
 	<section class="vh-100 ">
@@ -16,16 +17,18 @@
 					<div class="card fat border-0">
 						<div class="card-body">
 							<h4 class="card-title">Forgot Password</h4>
-							<form method="POST" class="my-login-validation" novalidate="">
+							<form method="POST" class="my-login-validation" novalidate="" action="{{ route('password.email') }}">
+
                                 @csrf
+
                                 @if (session('status'))
-                                    <div class="alert alert-sucess">
-                                        {{session('status')}}
+                                    <div class="alert alert-ssuccess">
+                                        {{ session('status')}}
                                     </div>
                                 @endif
 								<div class="form-group">
 									<label for="email">E-Mail Address</label>
-									<input id="email" type="email" class="form-control" name="email" value="{{_old('email')}}" required autofocus placeholder="Enter your email addressS">
+									<input id="email" type="email" class="form-control" name="email" value="{{ old('email')}}" required autofocus placeholder="Enter your email address">
 									<span class="text-danger">@error('email'){{$message}} @enderror</span>
 									<div class="form-text text-muted">
 										A Reset Password will be sent to your Email.
@@ -34,7 +37,7 @@
 
 								<div class="form-group m-0">
 									<button type="submit" class="btn btn-primary btn-block">
-										Reset Password
+										Email Reset Password link
 									</button>
 								</div>
 							</form>
--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -5,30 +5,46 @@
 	<meta charset="utf-8">
 	<meta name="author" content="Kodinger">
 	<meta name="viewport" content="width=device-width,initial-scale=1">
-	<title>My Login Page &mdash; Bootstrap 4 Login Page Snippet</title>
+	<title>Reset Password</title>
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-	<link rel="stylesheet" type="text/css" href="css/my-login.css">
+    <link rel="stylesheet" type="text/css" href="css/my-login.css">
 </head>
 <body class="my-login-page">
-	<section class="h-100">
+	<section class="vh-100">
 		<div class="container h-100">
 			<div class="row justify-content-md-center align-items-center h-100">
 				<div class="card-wrapper">
-					<div class="brand">
-						<img src="img/logo.jpg" alt="bootstrap 4 login page">
-					</div>
-					<div class="card fat">
+					<div class="card fat border-0">
 						<div class="card-body">
 							<h4 class="card-title">Reset Password</h4>
-							<form method="POST" class="my-login-validation" novalidate="">
+							<form method="POST" class="my-login-validation" novalidate="" action="{{ route('password.update') }}">
+                                @csrf
+
+                                <input type="hidden" name="token" value="{{ $token }}">
+                                <div class="form-group">
+									<label for="email">EMail</label>
+									<input id="email" type="email" class="form-control"
+                                    name="email" value="{{ $email ?? old('email') }}" required autofocus placeholder="Email address">
+									<span class="text-danger">@error('email'){{$message}} @enderror</span>
+                                </div>
+
 								<div class="form-group">
 									<label for="new-password">New Password</label>
-									<input id="new-password" type="password" class="form-control" name="password" required autofocus data-eye>
-									<div class="invalid-feedback">
-										Password is required
-									</div>
+									<input id="new-password" type="password" class="form-control"
+                                    name="password" required autofocus data-eye placeholder="Enter new password">
+                                    <span class="text-danger">@error('password'){{$message}} @enderror</span>
 									<div class="form-text text-muted">
 										Make sure your password is strong and easy to remember
+									</div>
+								</div>
+
+                                <div class="form-group">
+									<label for="password-confirm">Confirm Password</label>
+									<input id="password-confirm" type="password" class="form-control"
+                                    name="password_confirmation" required autofocus data-eye placeholder="Confirm your password">
+                                    <span class="text-danger">@error('password_confirmation'){{$message}} @enderror</span>
+									<div class="form-text text-muted">
+										Repeat your password
 									</div>
 								</div>
 
@@ -39,9 +55,6 @@
 								</div>
 							</form>
 						</div>
-					</div>
-					<div class="footer">
-						Copyright &copy; 2017 &mdash; Your Company
 					</div>
 				</div>
 			</div>
